b128132fbc36485bf1d42c36d97f1e9dc951277e
Brett Henderson <bretth18@gmail.com>
2016-04-12 11:26:57 -0700

    update compare.js

--- client/templates/compare/compare.js
+++ client/templates/compare/compare.js
@@ -4,18 +4,12 @@ Template.compare.helpers({
 
   },
   rendered: function(){
-    //TODO: on pageRendered, begin API call for drug interactionsAPI
-    function testJson() {
-    var baseUrl = 'https://rxnav.nlm.nih.gov/REST/';
-    var testUrl = 'https://rxnav.nlm.nih.gov/REST/rxcui?name=xanax'
-    var request = Meteor.npmRequire('request');
-    request(testUrl, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var info = JSON.parse(body)
-          console.log(body);
-        }
+    //call server-side function to grab api
+    //TODO: Pass client side inputs to function
+    Meteor.server({
+      getDrugJSON();
     });
-  }
+
 
   },
   destroyed: function(){
--- server/methods/interactions.js
+++ server/methods/interactions.js
@@ -1,45 +1,45 @@
 //handles grabbing our data from drug interactionsAPI
-Meteor.startup(function(){
-  Meteor.methods({
-    getDrugJSON:function(){
-      //two values are added to our GET request
-    function testJson() {
-      var baseUrl = 'https://rxnav.nlm.nih.gov/REST/';
-      var testUrl = 'https://rxnav.nlm.nih.gov/REST/rxcui?name=xanax'
-      var inteUrl = 'https://rxnav.nlm.nih.gov/REST/interaction?rxcui='
-      var request = Meteor.npmRequire('request');
-      //GET request to RESTapi
-    function getCui(){
-      request(testUrl, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            var info = JSON.parse(body);
-            console.log(info);
-          }
-          else {
-            console.log(err);
-          }
-      });
-    }
-      //get the rcxui for the drug
-      var rcxui1 = info.idGroup[rxnormid];
-      function getInteraction(){
-        //build our GET request
-        let finalUrl = `inteUrl{rxui1}&{rxui2}`; //ES6
-        request(finalUrl, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            var interJson = JSON.parse(body);
-            console.log(interJson);
-          }
-          else {
-            console.log(err);
-          }
-        });
-      }
+Meteor.startup(function() {
+    Meteor.methods({
+        getDrugJSON: function() {
+            //two values are added to our GET request
+            function testJson() {
+                var baseUrl = 'https://rxnav.nlm.nih.gov/REST/';
+                var testUrl = 'https://rxnav.nlm.nih.gov/REST/rxcui?name=xanax'
+                var inteUrl = 'https://rxnav.nlm.nih.gov/REST/interaction?rxcui='
+                var request = Meteor.npmRequire('request');
+                //GET request to RESTapi
+                function getCui() {
+                    request(testUrl, function(error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            var info = JSON.parse(body);
+                            console.log(info);
+                        } else {
+                            console.log(err);
+                        }
+                    });
+                }
+                //get the rcxui for the drug
+                var rcxui1 = info.idGroup[rxnormid];
+                var rcxui2 = info.idGroup[rxnormid];
 
-    }
+                function getInteraction() {
+                    //build our GET request
+                    let finalUrl = `inteUrl{rxui1}&{rxui2}`; //ES6
+                    request(finalUrl, function(error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            var interJson = JSON.parse(body);
+                            console.log(interJson);
+                        } else {
+                            console.log(err);
+                        }
+                    });
+                }
+
+            }
 
 
 
-    }
-  });
+        }
+    });
 });
