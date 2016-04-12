6b5f768027f0d63f89952b1eb488513c236c043e
Brett Henderson <bretth18@gmail.com>
2016-04-12 11:22:00 -0700

    updated interactions.js

--- server/methods/interactions.js
+++ server/methods/interactions.js
@@ -3,21 +3,39 @@ Meteor.startup(function(){
   Meteor.methods({
     getDrugJSON:function(){
       //two values are added to our GET request
-      function testJson() {
+    function testJson() {
       var baseUrl = 'https://rxnav.nlm.nih.gov/REST/';
       var testUrl = 'https://rxnav.nlm.nih.gov/REST/rxcui?name=xanax'
+      var inteUrl = 'https://rxnav.nlm.nih.gov/REST/interaction?rxcui='
       var request = Meteor.npmRequire('request');
       //GET request to RESTapi
+    function getCui(){
       request(testUrl, function (error, response, body) {
           if (!error && response.statusCode == 200) {
-            var info = JSON.parse(body)
+            var info = JSON.parse(body);
             console.log(info);
           }
+          else {
+            console.log(err);
+          }
       });
+    }
       //get the rcxui for the drug
       var rcxui1 = info.idGroup[rxnormid];
+      function getInteraction(){
+        //build our GET request
+        let finalUrl = `inteUrl{rxui1}&{rxui2}`; //ES6
+        request(finalUrl, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var interJson = JSON.parse(body);
+            console.log(interJson);
+          }
+          else {
+            console.log(err);
+          }
+        });
+      }
 
-      
     }
 
 
