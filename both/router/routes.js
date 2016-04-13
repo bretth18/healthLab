34738b1eaceea408c94ec463e9dbd29a5f4fdf1d
Brett Henderson <bretth18@gmail.com>
2016-04-12 20:09:21 -0700

    everything is broken and i want to die

--- both/router/routes.js
+++ both/router/routes.js
@@ -1,45 +1,47 @@
-Router.route('/', {
-  name: 'home',
-  controller: 'HomeController'
+FlowRouter.route('/', {
+  action: function() {
+  BlazeLayout.render( 'appLayout', { main: 'home' } );
+},
+
 });
 
-Router.route('/dashboard', {
-  name: 'dashboard',
-  controller: 'DashboardController'
-});
-
-Router.route('/items/new', {
-  name: 'items.new',
-  controller: 'ItemsNewController'
-});
-
-//router for profile template
-Router.route('/profile', {
-  name: 'profile'
-});
-
-Router.plugin('ensureSignedIn', {
-  only: ['dashboard']
-});
-
-
-//router for drugCompare
-Router.route('/compare', {
-  name: 'compare'
-});
-
-//router for privacy policy
-Router.route('/privacy', {
-  name: 'privacyPolicy'
-});
-
-//router for chat
-Router.route('/chat', {
-  waitOn: function () {
-    return IRLibLoader.load('//media.twiliocdn.com/sdk/rtc/js/ip-messaging/v0.8/twilio-ip-messaging.min.js');
-    if (err){
-      return Meteor.console.error('twilio connection failed');
-    }
-  },
-  name: 'chat'
-});
+// FlowRouter.route('/dashboard', {
+//   name: 'dashboard',
+//   controller: 'DashboardController'
+// });
+//
+// FlowRouter.route('/items/new', {
+//   name: 'items.new',
+//   controller: 'ItemsNewController'
+// });
+//
+// //router for profile template
+// FlowRouter.route('/profile', {
+//   name: 'profile'
+// });
+//
+// FlowRouter.plugin('ensureSignedIn', {
+//   only: ['dashboard']
+// });
+//
+//
+// //router for drugCompare
+// FlowRouter.route('/compare', {
+//   name: 'compare'
+// });
+//
+// //router for privacy policy
+// FlowRouter.route('/privacy', {
+//   name: 'privacyPolicy'
+// });
+//
+// //router for chat
+// FlowRouter.route('/chat', {
+//   waitOn: function () {
+//     return IRLibLoader.load('//media.twiliocdn.com/sdk/rtc/js/ip-messaging/v0.8/twilio-ip-messaging.min.js');
+//     if (err){
+//       return Meteor.console.error('twilio connection failed');
+//     }
+//   },
+//   name: 'chat'
+// });
