09deb01038be2cf8342045333909bd3a7420a8c2
Brett Henderson <bretth18@gmail.com>
2016-04-12 20:23:04 -0700

    revert to 6b5f7

--- client/templates/compare/compare.js
+++ client/templates/compare/compare.js
@@ -4,12 +4,18 @@ Template.compare.helpers({
 
   },
   rendered: function(){
-    //call server-side function to grab api
-    //TODO: Pass client side inputs to function
-    Meteor.server({
-      getDrugJSON();
+    //TODO: on pageRendered, begin API call for drug interactionsAPI
+    function testJson() {
+    var baseUrl = 'https://rxnav.nlm.nih.gov/REST/';
+    var testUrl = 'https://rxnav.nlm.nih.gov/REST/rxcui?name=xanax'
+    var request = Meteor.npmRequire('request');
+    request(testUrl, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var info = JSON.parse(body)
+          console.log(body);
+        }
     });
-
+  }
 
   },
   destroyed: function(){
